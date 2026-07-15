cf35d17ff3b2a61aa2ea5f8d7a1871c596f1e48a
perezm89 <perezm.brayn1418@gmail.com>
2026-07-14 23:14:47 -0400

    Add GPIO driver interface and update gitignore

---
+++ drivers/gpio.c
@@ -0,0 +1,22 @@
+#include "gpio.h"
+#include <msp430.h>
+
+void GPIO_init(void)
+{
+
+}
+
+void LED_green_on(void)
+{
+
+}
+
+void LED_green_off(void)
+{
+
+}
+
+void LED_green_toggle(void)
+{
+
+}
---
+++ include/gpio.h
@@ -0,0 +1,10 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+void GPIO_init(void);
+
+void LED_green_on(void);
+void LED_green_off(void);
+void LED_green_toggle(void);
+
+#endif
